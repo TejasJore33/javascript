2d56df28bf725f7034a382383cba3f45d8745991
TejasJore33 <tejasjore33@Gmail.com>
2023-05-05 09:48:31 +0530

    intro to js
    intro to js

---
+++ 01_variable_assign.js
@@ -0,0 +1,29 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName="Tejas Dattatray Jore";
+console.log(myFullName);
+
+var myLove="I LOVE ONLY JS";
+console.log(myLove)
+
+var firstName="Tejas";
+console.log(firstName);
+
+var lastName="Jore";
+console.log(lastName);
+
+var age="21";
+console.log(age);
+
+var address="At- pathare, Tal-sinnar, Dist-Nashik";
+console.log(address);
+
+var pincode = "422104";
+console.log(pincode);
+
+var gender ="Male";
+console.log(gender);
+
+var isMarried="No";
+console.log(isMarried);
---
+++ hello.js
@@ -0,0 +1,23 @@
+console.log("AT- PATHARE, tal-sinnar , Dist-nashik");
+
+var firstName = "Tejas";
+
+var secondNames ="Jore"
+console.log(firstName +" "+secondNames);
+
+console.log("Tejas Jore..");
+
+var firstName = "Tejas";
+console.log(firstName);
+
+var age; //Variable Declaration 
+age = "22";  //Variable Initialization
+console.log(age);
+
+var holder; //Variable Declaration
+console.log(holder); //undefined
+holder = "My Spec"; //Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; //Variable update
+console.log(holder); //
