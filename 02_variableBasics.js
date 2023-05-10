7400deb04b7e6dcd5046cd236a03a0e450a53be0
TejasJore33 <tejasjore33@Gmail.com>
2023-05-10 11:57:07 +0530

    variable basics
    variable basics

---
+++ 02_variableBasics.js
@@ -0,0 +1,9 @@
+var number=203;
+var abc=typeof number;
+console.log("number:",number,abc);
+var address ="pathare";
+var def=typeof add;
+console.log("address:",address,def);
+var tf=true;
+var ghi= typeof tf;
+console.log("boolean value is:",tf,ghi);
---
+++ 03_typeOf.js
@@ -0,0 +1,17 @@
+var bankName ="SBI Bank"
+console.log(bankName);
+var abc = typeof bankName;
+var marks='90';
+var def= typeof marks;
+console.log(def);
+var isWorking;
+console.log(isWorking);
+isWorking=true;
+console.log(isWorking);
+var totalCount="Hundred and Seven";
+var xyz=typeof totalCount;
+console.log(totalCount);
+totalCount=107;
+var mno= typeof totalCount;
+console.log(totalCount);
+console.log(mno);
