154ebf57213369730680f82c082f32084a3c857a
TejasJore33 <tejasjore33@Gmail.com>
2023-05-11 11:30:07 +0530

    swap variables using temp variable
    swap variables using temp variable

---
+++ 02_swapVariables.js
@@ -0,0 +1,20 @@
+console.log("-----SWAP TWO VARIABLE VALUES USING THIRD VARIABLE-----");
+var sweety="Sweety";
+var cutie="Cutie";
+console.log("Before swap :" , sweety ,cutie);
+temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("After swap :", sweety , cutie);
+
+console.log("-----SWAP THREE VARIABLE VALUES------");
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Before swap : ", num1, num2, num3);
+var temp= num1;
+var num1=num2;
+var num2=num3;
+var num3=temp;
+console.log("After swap:",num1, num2, num3);
