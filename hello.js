25eb742ae3f0ac361d92fae3aa1b5cb602c17b50
TejasJore33 <tejasjore33@Gmail.com>
2023-05-11 08:49:03 +0530

    intro to functions
    intro to functions

--- hello.js
+++ hello.js
@@ -17,7 +17,7 @@ console.log(age);
 var holder; //Variable Declaration
 console.log(holder); //undefined
 holder = "My Spec"; //Variable Initialization
-console.log(holder);
+console.log(holder);    
 
 holder = "Sachin Spec"; //Variable update
 console.log(holder); //
