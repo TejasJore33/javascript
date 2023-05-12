a084c52f566552ee240bad0d0be0f25e5456a553
TejasJore33 <tejasjore33@Gmail.com>
2023-05-12 09:35:50 +0530

    string basics
    string basics

---
+++ 04_stringBasics.js
@@ -0,0 +1,15 @@
+function stringBasics(){
+    var company='my dream company is "Google"';
+    console.log(company);
+}
+stringBasics();
+var hobby1="driving";
+var hobby2="coding";
+var hobby3="music";
+console.log("My hobbies are:", hobby1,hobby2,hobby3);
+var hobby1Length=hobby1.length;
+var hobby2Length=hobby2.length;
+var hobby3Length=hobby3.length;
+var totalCharacter=(hobby1Length+hobby2Length+hobby3Length);
+console.log("The total characters in hobby1,hobby2 & hobby3 is:",totalCharacter);
+
