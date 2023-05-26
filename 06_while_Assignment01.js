f571f029bf9ec7c6d8ee679e6ca20d9b24f6d512
TejasJore33 <tejasjore33@Gmail.com>
2023-05-26 20:30:46 +0530

    while loop
    while loop

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,51 @@
+// var i=5;
+// while (i<=15) {
+//     console.log(i);
+//     i++;
+// }
+
+// var j=50;
+// while (j>=40) {
+//     console.log(j);
+//     j--;
+// }
+
+
+// var i=0;
+// var count=1;
+// while (count<=15) {
+//     if (i % 2 !==0) {
+//         console.log(i);
+//         count++; 
+//     }
+//     i++;
+// }
+
+// var j=0;
+// count=1;
+// while (count<=10) {
+//     if (j%2==0) {
+//         console.log(j);
+//         count++;
+//     }
+// j++;
+// }
+
+// var number=5;
+// while (number<=50) {
+//     console.log(number);
+//     number+=5;
+// }
+
+// var number=10;
+// while (number<=100) {
+//     console.log(number);
+//     number+=10;
+// }
+
+var number=100;
+while (number>=10) {
+    console.log(number);
+    number-=10;
+}
+
