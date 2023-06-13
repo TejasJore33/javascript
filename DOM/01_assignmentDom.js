24e6ea34be909c65dbd11ff2a92d645c5729a625
TejasJore33 <tejasjore33@Gmail.com>
2023-06-13 23:56:32 +0530

    DOM
    DOM

---
+++ DOM/01_assignmentDom.js
@@ -0,0 +1,8 @@
+console.log('changing the color to green');
+const profile = document.querySelector('#profile');
+profile.style.color='green';
+
+console.log('removing element form list');
+const deleteElement= document.querySelector('ul');
+const removeElement=document.querySelector('#angular');
+deleteElement.removeChild(removeElement);
