f72bc726e5a815a9c98095a41baed6e47b3e4295
TejasJore33 <tejasjore33@Gmail.com>
2024-03-26 09:33:41 +0530

    IntroTo Funciton

---
+++ functionIntro.js
@@ -0,0 +1,19 @@
+ // functions in javascript
+
+ // Funciton Decleration
+
+ function sum(a,b) {
+    return a+b;
+ }
+
+ const sum1 = sum(50,6);
+console.log(sum1);
+
+
+// Funtion Expression
+
+const funciton =function (a,b) {
+    return a*b;
+}
+
+console.log(funciton(5,6));
