4014f564f7563dc49fd5aac5328334bc753b2485
TejasJore33 <tejasjore33@Gmail.com>
2025-01-09 21:43:27 +0530

    sets

---
+++ sets.js
@@ -0,0 +1,15 @@
+// Create a Set
+const letters = new Set();
+
+// Create Variables
+const a = "a";
+const b = "b";
+const c = "c";
+
+// Add Values as Variables to the Set
+letters.add(a);
+letters.add(b);
+letters.add(c);
+
+// Display the Size
+console.log(`the size of array is ${letters.size}`);
