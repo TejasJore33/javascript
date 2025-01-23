9a828b613cd3f8d34fe7b2732125f25edc42ef28
Tejas Jore <116269768+TejasJore33@users.noreply.github.com>
2025-01-23 16:44:36 +0530

    Promises

---
+++ promises.js
@@ -0,0 +1,22 @@
+function savetoDb(data) {
+    return new Promise((resolve,reject)=>{
+        let internetSpeed = Math.floor(Math.random()*10) +1;
+        if(internetSpeed>4){
+            resolve("Sucess : data was saved ");
+        }else{
+            reject("Failure : weak connection");
+        }
+    });
+}
+
+savetoDb("Tejas Jore")
+.then(()=>{
+    console.log("data1 saved");
+    return savetoDb("Tejas");
+})
+.then(()=>{
+    console.log("Data2 is saved");
+})
+.catch(()=>{
+    console.log("Promise was rejected");
+});
